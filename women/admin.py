d6da51e03a10bd2597024cd8044e7ea94c4f2202
Varyag-Ingvar <varyag8512@gmail.com>
2022-12-21 20:30:08 +0400

    Multiple changes in models.py, admin.py, index.html. New DB migrations.

--- women/admin.py
+++ women/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# импортируем модели для работы с ними в админ-панели
+from .models import *
+
+
+class WomenAdmin(admin.ModelAdmin):
+    """класс котрый позволяет отобразить нужные поля модели Women в админке"""
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')  # поля отображаемые в админке
+    list_display_links = ('id', 'title')  # поля, на которые мы можем кликнуть и перейти в соотв.статью
+    search_fields = ('title', 'content')  # поля, по которым мы можем осуществлять поиск
+    list_editable = ('is_published',)   # список полей котрые можно редактировать (передается кортеж!!!)
+    list_filter = ('title', 'is_published', 'time_create')  # поля, по которым можно фильтровать статьи в админке
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """класс котрый позволяет отобразить нужные поля модели Category в админке"""
+    list_display = ('id', 'name')  # поля отображаемые в админке
+    list_display_links = ('id', 'name')  # поля, на которые мы можем кликнуть и перейти в соотв.статью
+    search_fields = ('name',)  # поля, по которым мы можем осуществлять поиск (кортеж! нужна запятая если один элемент)
+
+
+# регистрируем модели в админке (сначала саму модель, уже затем админ-класс)
+admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
--- women/models.py
+++ women/models.py
@@ -6,21 +6,21 @@ class Women(models.Model):
     """Создаем модель (таблицу в БД) Women"""
     # поле id создается автоматически, SQL-запросы можно посмотреть в консоли командой -
     # python manage.py sqlmigrate women 0001, где women - название приложения, 0001 - номер файла миграции.
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=255, verbose_name='Заголовок')  # параметр verbose_name отображает название в админке
+    content = models.TextField(blank=True, verbose_name='Текст статьи')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d", verbose_name='Фото')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    is_published = models.BooleanField(default=True, verbose_name='Статус публикации')
     # поле для связи "один ко многим" с таблицей Category, к cat джанго автоматом добавит _id и поле будет cat_id
     # ссылаемся на модель Category и защищаем запись в таблице women от удаления, при удалении записи из Category
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         """при чтении записи, вместо ссылки на объект будет возвращать заголовок(title) записи"""
         return self.title
 
-    def get_absolute_url(self):
+    def get_absolute_url(self):  # метод именно с таким названием позволяет отобразить кнопку в админке - view on site
         """метод формирует динамический маршрут к конкретной записи из БД по ее id (primary key - pk),
         у экзепляра класса Women берем параметр pk (self.pk)
         и динамически присваиваем его в маршрут (в параметр post_id),
@@ -29,10 +29,17 @@ class Women(models.Model):
         при нажатии кнопки 'Читать пост'"""
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        """Специальный вложенный класс, который используется в админ-панели для настройки данной модели
+        и управления ею"""
+        verbose_name = 'Известные женщины'  # эти названия будут отображаться в админке
+        verbose_name_plural = 'Известные женщины'
+        ordering = ['time_create', 'title']   # сортируем записи в админке по времени создания и потом по заголовку
+
 
 class Category(models.Model):
     """Создаем модель (таблицу в БД) Category"""
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
 
     def __str__(self):
         """при чтении записи, вместо ссылки на объект будет возвращать имя (name) записи (Актрисы, Певицы и т.д.)"""
@@ -40,6 +47,13 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        """Специальный вложенный класс, который используется в админ-панели для настройки данной модели
+        и управления ею"""
+        verbose_name = 'Категории'  # эти названия будут отображаться в админке
+        verbose_name_plural = 'Категории'
+        ordering = ['name']   # сортируем записи в админке по названию
 
 
 
